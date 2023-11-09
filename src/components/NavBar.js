df5b6afc20c3b4ff8648ac6d60b2c5dfcb862dca
michael-korir <michaelkiptoo2000@gmail.com>
2023-11-09 11:44:52 +0300

    completed react hooks assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+  const navList =links.map((link,index) => {
+    return <a key={index} href={`#${link}`}>{link}</a>
+  });
+
+  return <nav>{navList}
+  </nav>
+};
 
 export default NavBar;
